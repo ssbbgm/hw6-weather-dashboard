5c8838cb0c9b863a6ec3a89c06b40d5f0742d7be
Sherreka Burton <rekadarnb@gmail.com>
2022-04-03 19:28:18 -0400

    got current weather data and displayed it

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,10 @@ let pastCitiesEl = document.getElementById('past-searches');
 let fiveDayEl = document.getElementById('searched-city-five-day');
 let futureWeatherEl = document.getElementById('future-weather-data');
 
-//Time Element
-var date = moment().format("MMM Do, YYYY");
 
 //Global Variables
 
 let cities = [];
-let BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q="
 
 //Form Submission action
 $(searchBtnEl).on ('click', function (event) {
@@ -34,9 +31,9 @@ $(searchBtnEl).on ('click', function (event) {
 //Get the current weather data
 
 var getCurrentWeather = function (city) {
-    let currentWeatherAPIKey = 'e4bf4f9f84d50f60c4906ff3e10373be';
+    let apiKey = 'e4bf4f9f84d50f60c4906ff3e10373be';
 
-    let apiUrl = `${BASE_URL}${city}&units=imperial&appid=${currentWeatherAPIKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     console.log(apiUrl);
   
     fetch(apiUrl)
@@ -57,35 +54,28 @@ var getCurrentWeather = function (city) {
 
 //Display results
 
-var displayCityData = function (weather, searchCity) {
+let displayCityData = function (weather, searchCity) {
+    console.log(weather);
 
+    $("#searched-city").html(weather.name);
 
+    const retrievedDate = new Date(weather.dt * 1000);
 
-  
-    // for (var i = 0; i < repos.length; i++) {
-    //   var repoName = repos[i].owner.login + '/' + repos[i].name;
-  
-    //   var repoEl = document.createElement('div');
-    //   repoEl.classList = 'list-item flex-row justify-space-between align-center';
-  
-    //   var titleEl = document.createElement('span');
-    //   titleEl.textContent = repoName;
-  
-    //   repoEl.appendChild(titleEl);
-  
-    //   var statusEl = document.createElement('span');
-    //   statusEl.classList = 'flex-row align-center';
-  
-    //   if (repos[i].open_issues_count > 0) {
-    //     statusEl.innerHTML =
-    //       "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    //   } else {
-    //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    //   }
-  
-    //   repoEl.appendChild(statusEl);
-  
-    //   repoContainerEl.appendChild(repoEl);
+    $("#current-date").html(
+        retrievedDate.getMonth() +
+          "/" +
+          retrievedDate.getDate() +
+          "/" +
+          retrievedDate.getFullYear()
+      );
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('src', ` http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+    $('#current-icon').append(weatherIcon);
+
+    $("#temp").html(`Temp: ${weather.main.temp}&deg;F`);
+    $("#wind").html(`Wind: ${weather.wind.speed} MPH`);
+    $("#humidity").html(`Humidity: ${weather.main.humidity} %`);
+
     }
- 
-  
+
