ded4c92305808aa3f0c023f5cacbb9dec2f9acef
Sherreka Burton <rekadarnb@gmail.com>
2022-04-03 16:39:57 -0400

    added moment JS to file

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ let pastCitiesEl = document.getElementById('past-searches');
 let fiveDayEl = document.getElementById('searched-city-five-day');
 let futureWeatherEl = document.getElementById('future-weather-data');
 
+//Time Element
+var date = moment().format("MMM Do, YYYY");
+
 //Global Variables
 
 let cities = [];
@@ -22,7 +25,6 @@ $(searchBtnEl).on ('click', function (event) {
     if (city) {
       getCurrentWeather(city);
   
-      searchedCityEl.textContent = '';
       cityInputEl.value = '';
     } else {
       alert('Please enter a city');
@@ -34,7 +36,7 @@ $(searchBtnEl).on ('click', function (event) {
 var getCurrentWeather = function (city) {
     let currentWeatherAPIKey = 'e4bf4f9f84d50f60c4906ff3e10373be';
 
-    let apiUrl = `${BASE_URL}${city}&appid=${currentWeatherAPIKey}`;
+    let apiUrl = `${BASE_URL}${city}&units=imperial&appid=${currentWeatherAPIKey}`;
     console.log(apiUrl);
   
     fetch(apiUrl)
@@ -55,5 +57,35 @@ var getCurrentWeather = function (city) {
 
 //Display results
 
+var displayCityData = function (weather, searchCity) {
 
+
+
+  
+    // for (var i = 0; i < repos.length; i++) {
+    //   var repoName = repos[i].owner.login + '/' + repos[i].name;
+  
+    //   var repoEl = document.createElement('div');
+    //   repoEl.classList = 'list-item flex-row justify-space-between align-center';
+  
+    //   var titleEl = document.createElement('span');
+    //   titleEl.textContent = repoName;
+  
+    //   repoEl.appendChild(titleEl);
+  
+    //   var statusEl = document.createElement('span');
+    //   statusEl.classList = 'flex-row align-center';
+  
+    //   if (repos[i].open_issues_count > 0) {
+    //     statusEl.innerHTML =
+    //       "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+    //   } else {
+    //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    //   }
+  
+    //   repoEl.appendChild(statusEl);
+  
+    //   repoContainerEl.appendChild(repoEl);
+    }
+ 
   
